b1a0bf3fbcf3eac2e3a88e71492f787eaf3e09b1
linjw <bluesky466@qq.com>
2019-08-24 18:11:45 +0800

    [feature] finish camera perview

---
+++ app/src/main/java/me/islinjw/shadercamera/CameraCapturer.java
@@ -0,0 +1,158 @@
+package me.islinjw.shadercamera;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CameraCapturer {
+    private static final String TAG = "CameraCapturer";
+
+    private Context mContext;
+    private Handler mHandler;
+
+    private CameraDevice mCameraDevice;
+    private Surface mPreviewSurface;
+
+    private CameraDevice.StateCallback mOpenCameraCallback =
+            new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(CameraDevice camera) {
+                    openCameraSession(camera);
+                }
+
+                @Override
+                public void onDisconnected(CameraDevice camera) {
+                }
+
+                @Override
+                public void onError(CameraDevice camera, int error) {
+                }
+            };
+
+    private CameraCaptureSession.StateCallback mCreateSessionCallback =
+            new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    requestPreview(session);
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            };
+
+    public CameraCapturer(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    @SuppressLint("MissingPermission")
+    public void openCamera(
+            SurfaceTexture preview,
+            int facing,
+            int width,
+            int height,
+            Handler handler) {
+        mPreviewSurface = new Surface(preview);
+        mHandler = handler;
+
+        CameraManager manager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            for (String id : manager.getCameraIdList()) {
+                CameraCharacteristics cc = manager.getCameraCharacteristics(id);
+                if (cc.get(CameraCharacteristics.LENS_FACING) == facing) {
+                    manager.openCamera(id, mOpenCameraCallback, handler);
+                    Size[] sizes = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                            .getOutputSizes(SurfaceTexture.class);
+                    setMostSuitableSize(preview, sizes, width, height);
+                    break;
+                }
+            }
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "can not open camera", e);
+        }
+    }
+
+    public void closeCamera() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private void setMostSuitableSize(
+            SurfaceTexture surfacePreview,
+            Size[] sizes,
+            float width,
+            float height) {
+
+        float targetRatio = height / width;
+        Size result = null;
+        for (Size size : sizes) {
+            if (result == null || isMoreSuitable(result, size, targetRatio)) {
+                result = size;
+            }
+        }
+        if (result == null) {
+            return;
+        }
+
+        surfacePreview.setDefaultBufferSize(result.getWidth(), result.getHeight());
+    }
+
+    private boolean isMoreSuitable(Size current, Size target, float targetRatio) {
+        if (current == null) {
+            return true;
+        }
+        float dRatioTarget = Math.abs(targetRatio - getRatio(target));
+        float dRatioCurrent = Math.abs(targetRatio - getRatio(current));
+        return dRatioTarget < dRatioCurrent
+                || (dRatioTarget == dRatioCurrent && getArea(target) > getArea(current));
+    }
+
+    private int getArea(Size size) {
+        return size.getWidth() * size.getHeight();
+    }
+
+    private float getRatio(Size size) {
+        return ((float) size.getWidth()) / size.getHeight();
+    }
+
+    private void openCameraSession(CameraDevice camera) {
+        mCameraDevice = camera;
+        try {
+            List<Surface> outputs = Arrays.asList(mPreviewSurface);
+            camera.createCaptureSession(outputs, mCreateSessionCallback, mHandler);
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "createCaptureSession failed", e);
+        }
+    }
+
+    private void requestPreview(CameraCaptureSession session) {
+        if (mCameraDevice == null) {
+            return;
+        }
+        try {
+            CaptureRequest.Builder builder = mCameraDevice.createCaptureRequest(
+                    CameraDevice.TEMPLATE_PREVIEW);
+            builder.addTarget(mPreviewSurface);
+            session.setRepeatingRequest(builder.build(), null, mHandler);
+        } catch (CameraAccessException e) {
+            Log.e(TAG, "requestPreview failed", e);
+        }
+    }
+}
--- app/src/main/java/me/islinjw/shadercamera/MainActivity.java
+++ app/src/main/java/me/islinjw/shadercamera/MainActivity.java
@@ -1,5 +1,8 @@
 package me.islinjw.shadercamera;
 
+import android.Manifest;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCharacteristics;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.TextureView;
@@ -7,12 +10,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
+import io.reactivex.functions.Consumer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
     @Bind(R.id.preview)
     TextureView mPreview;
 
@@ -22,12 +28,24 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.switch_camera)
     Button mSwitchCamera;
 
+    private CameraCapturer mCameraCapturer;
+
+    private SurfaceTexture mSurfaceTexture;
+
+    private boolean mOpenCamera = false;
+    private int mCameraFacing = CameraCharacteristics.LENS_FACING_BACK;
+
+    private RxPermissions mRxPermissions = new RxPermissions(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        mPreview.setSurfaceTextureListener(this);
     }
 
     @OnCheckedChanged(R.id.record)
@@ -36,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.switch_camera)
     public void switchCamera() {
+        mCameraFacing = mCameraFacing == CameraCharacteristics.LENS_FACING_BACK
+                ? CameraCharacteristics.LENS_FACING_FRONT : CameraCharacteristics.LENS_FACING_BACK;
+
+        closeCamera();
+        openCamera();
     }
 
     @Override
@@ -43,17 +66,74 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         makeFullscreen();
+
+        if (mCameraCapturer != null && !mOpenCamera) {
+            openCamera();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeCamera();
     }
 
     private void makeFullscreen() {
         getWindow().getDecorView()
-            .setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            );
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                );
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(final SurfaceTexture surface, final int width, final int height) {
+        mSurfaceTexture = surface;
+        mRxPermissions
+                .request(Manifest.permission.CAMERA)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean granted) {
+                        if (granted) {
+                            mCameraCapturer = new CameraCapturer(MainActivity.this);
+                            openCamera();
+                        }
+                    }
+                });
+    }
+
+    private void openCamera() {
+        mOpenCamera = true;
+
+        mCameraCapturer.openCamera(
+                mSurfaceTexture,
+                mCameraFacing,
+                getResources().getDisplayMetrics().widthPixels,
+                getResources().getDisplayMetrics().heightPixels,
+                null);
+    }
+
+    private void closeCamera() {
+        mOpenCamera = false;
+        mCameraCapturer.closeCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
